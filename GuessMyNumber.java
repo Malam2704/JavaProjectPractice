cb97e8f7927dad3dafb2507fa27cf8d60bd72b93
Mohammed Alam <ma3655@rit.edu>
2024-12-13 03:14:53 -0500

    Java Guess my number

---
+++ GuessMyNumber.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class GuessMyNumber {
+
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        Random rand = new Random();
+        int systemRandomNumber = rand.nextInt(101) - 1;
+
+        System.out.println("Enter a number from 1-100: ");
+        int userInt = myScanner.nextInt();
+        System.out.println("User's number is " + userInt);
+
+        int tries = 1;
+        while (true) {
+            if (userInt > systemRandomNumber) {
+                System.out.println("Your guessed number is greater, try again: ");
+                userInt = myScanner.nextInt();
+                tries++;
+            } else if (userInt < systemRandomNumber) {
+                System.out.println("Your guessed number is lower, try again: ");
+                userInt = myScanner.nextInt();
+                tries++;
+            } else if (userInt == systemRandomNumber) {
+                System.out.println("Your guessed number Correct! ");
+                break;
+            }
+        }
+
+        System.out.println("You got the correct answer in " + tries + " guesses!");
+        myScanner.close();
+    }
+}
