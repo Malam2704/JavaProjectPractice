6f8c7a01c23e5dd4b5976c7bb8d1c6755e254200
Mohammed <ma3655@g.rit.edu>
2024-12-24 21:29:18 -0500

    yeehaw

--- BasicMMO/Game.java
+++ BasicMMO/Game.java
@@ -4,5 +4,6 @@ public class Game {
     public static void main(String[] args) {
         Player p1 = new Player("Joe", 200, 20, "example first player");
         Player p2 = new Player("Max", 200, 20, "example second player");
+
     }
 }
