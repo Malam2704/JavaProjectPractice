59f2758c23555a357c62e3d4b3b0ace1942db8a6
Mohammed <ma3655@g.rit.edu>
2024-12-25 23:46:55 -0500

    clean up errors

--- BasicMMO/Game.java
+++ BasicMMO/Game.java
@@ -1,13 +1,13 @@
 package BasicMMO;
 
 public class Game {
-    void
+
     public static void main(String[] args) {
         Player p1 = new Player("Joe", 200, 20, "example first player");
         Player p2 = new Player("Max", 200, 20, "example second player");
 
-        while(p1.getHealth() > 0 && p2.getHealth() > 0){
-            
+        while (p1.getHealth() > 0 && p2.getHealth() > 0) {
+
         }
     }
 }
